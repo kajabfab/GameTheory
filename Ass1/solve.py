240f633792c41ad553ec03219abe69e8c472cd0f
kajabfab <vishal.bfab@gmail.com>
2016-08-30 22:50:02 +0530

    "Sorted all strategy profiles of all players in to lists"

--- Ass1/solve.py
+++ Ass1/solve.py
@@ -5,11 +5,6 @@ if len(argv) == 2:
 else:
     FN = str(raw_input("Enter Input File: "))
 
-
-def validate(file):
-    pass
-
-
 F = open(FN, 'r')
 headers = F.readline().strip()
 header = headers.split()
@@ -24,16 +19,18 @@ else:
 
     info = F.readline().strip().split('} {')
 
-    players = info[0].strip('{ }').split()
-    player = []
-    for p in players:
+    players_str = info[0].strip('{ }').split()
+    player = []  # player names
+    for p in players_str:
         player.append(p.strip('"'))
+
+    NP = len(player)  # no of players
 
     # print player
 
-    strategies = info[1].strip('{ }').split()
-    strategy = []
-    for s in strategies:
+    strategy_str = info[1].strip('{ }').split()
+    strategy = []  # no of strategies of player i
+    for s in strategy_str:
         strategy.append(int(s))
 
     # print strategy
@@ -47,4 +44,32 @@ else:
 
     # print payoff
 
-    utility = []
+    strategy_profiles = []
+
+    NSP = 1  # no of strategy profiles
+    for n in strategy:
+        NSP *= n
+
+    # print NSP
+
+    for i in range(0, NSP):
+        strategy_profiles.append(tuple(payoff[i*NP:(i+1)*NP]))
+
+    # print strategy_profiles
+    
+    for i in range(0, NP):
+        NPS = strategy[i]  # number of player strategies
+
+        player_strategy = []
+        INC = 1
+        for j in range(0, i):
+            INC *= strategy[j]
+
+        if i == 0:  # P1
+            for j in range(0, NSP/NPS):
+                player_strategy.append(strategy_profiles[j*NPS:(j+1)*NPS])
+        else:
+            for j in range(0, NSP/NPS):
+                player_strategy.append(strategy_profiles[j:j+(INC*NPS):INC])
+
+        # print i+1, INC, player_strategy
